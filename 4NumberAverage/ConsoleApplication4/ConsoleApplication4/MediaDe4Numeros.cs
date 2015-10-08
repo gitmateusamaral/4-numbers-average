3368c8923c4b3928f7e1a3218622de99b6247f09
Mateus Amaral <reiclick123@hotmail.com>
2015-10-08 18:32:21 -0300

    4 Number Average
    Program to discover the mean of 4 numbers using Visual Studio Console

---
+++ 4NumberAverage/ConsoleApplication4/ConsoleApplication4/MediaDe4Numeros.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,y,z,v;
+            Console.WriteLine("Escreva um número: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva outro número: ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva mais um número: ");
+            z = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mais um número: ");
+            v = int.Parse(Console.ReadLine());
+            int t = x + y + z + v;
+            int m = (x + y + z + v) / 4;
+            Console.WriteLine("Total: {0}, Média: {1}", t,m);
+            Console.WriteLine("Pressione Enter para sair");
+            Console.Read();
+        }
+    }
+}
---
+++ 4NumberAverage/ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+            int v;
+            Console.WriteLine("Escreva um número: ");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva outro número: ");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva mais um número: ");
+            z = int.Parse(Console.ReadLine());
+            Console.WriteLine("Mais um número: ");
+            v = int.Parse(Console.ReadLine());
+            Console.WriteLine("Resultado: " + (x + y + z + v)/4);
+            Console.WriteLine("Pressione Enter para sair");
+            Console.Read();
+        }
+    }
+}
